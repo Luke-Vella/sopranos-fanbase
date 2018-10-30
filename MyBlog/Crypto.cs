cb8dd8f9837c7f7a90f882c70cbe22856f5510c2
Luke Vella <luke.vella.0697@gmail.com>
2018-10-30 16:38:20 +0100

    Registration Styling Commit

--- MyBlog/Crypto.cs
+++ MyBlog/Crypto.cs
@@ -12,8 +12,7 @@ namespace MyBlog
         {
             return Convert.ToBase64String(
                 System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(value)));
-
-            //haha neil gay.
+            
         }
     }
 }
